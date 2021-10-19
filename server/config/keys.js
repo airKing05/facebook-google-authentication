17aab73e30db99f702f71257b06f895bdb29bf11
anilraj <anil050797@gmial.com>
2021-10-20 01:57:51 +0530

    google auth update 01

---
+++ server/config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+    googleClientID: `713832927530-qghrufr2e8bfqrmu4o9qa0qqd2ldt9tg.apps.googleusercontent.com`,
+    googleClientSecret: `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`,
+};
